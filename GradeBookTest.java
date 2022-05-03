a3115cb019a29ddd36d1251ea851b47585783903
Gilson Carrara <102890937+gilcarrara@users.noreply.github.com>
2022-05-02 21:36:41 -0300

    String test
    String test

---
+++ GradeBookTest.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+
+public class GradeBookTest {
+	
+public static void main(String args[]) {
+		
+		GradeBook gradeBook1 = new GradeBook("biologia");
+		GradeBook gradeBook2 = new GradeBook("Linguagem Java");
+		
+		System.out.printf("course 1 name is: %s\n\n", gradeBook1.getCourseName());
+		System.out.printf("course 2 name is: %s\n\n", gradeBook2.getCourseName());
+		
+		// myGradeBook.displayMessage();
+
+	}
+
+
+}
